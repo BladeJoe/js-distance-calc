bcd0fd5cf76901fb96e79b5caecd61aff0a30af6
Joe <106337992+BladeJoe@users.noreply.github.com>
2022-07-14 20:02:06 +0500

    new

--- main.js
+++ main.js
@@ -5,23 +5,29 @@ var planeeTime = document.querySelector("#planeTime");
 
 var form = document.querySelector("#form");
 form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-    var input = document.querySelector("#input");
-    
-    if (!isNaN(input.value) && input.value <= 9999999999999 && input.value > 0) {
+       evt.preventDefault();
+       var input = document.querySelector("#input");
 
-        walkTime.value = `${Math.floor((input.value / 3.2).toFixed(2))} Hours
- ${(((( input.value / 3.2).toFixed(2) % 1 )%100)*60).toFixed(0) } mins`;
-        bikeTime.value = `${Math.floor((input.value / 20.1).toFixed(2))} Hours
- ${(((( input.value / 20.1).toFixed(2) % 1 )%100)*60).toFixed(0) } mins`;
-        carTime.value = `${Math.floor((input.value / 80).toFixed(2))} Hours
- ${(((( input.value / 80).toFixed(2) % 1 )%100)*60).toFixed(0) } mins`;
-        planeeTime.value = `${Math.floor((input.value / 500).toFixed(2))} Hours
- ${(((( input.value / 500).toFixed(2) % 1 )%100)*60).toFixed(0) } mins`;
-    } else {
-        alert("Enter a number")
-    } 
+       if (!isNaN(input.value) && input.value > 0) {
 
+              walkTime.value = time(input.value,3.6)
+              bikeTime.value = time(input.value,20.1)
+              carTime.value = time(input.value,70)
+              planeeTime.value = time(input.value,800) 
+       } else {
+              alert("Enter a number")
+       }
+
+       function time(a, b) {
+              var string = `${Math.floor((a / b)/24).toFixed(0) } days 
+${Math.floor((a / b).toFixed(2))%24 } Hours
+ ${(((( a / b).toFixed(2) % 1 )%100)*60).toFixed(0) } mins`;
+
+
+
+              return string;
+
+       }
 
 
 })
